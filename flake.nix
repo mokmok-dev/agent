6e2651f6e9577e8324b120bb32d37bc425510eb6
pranc1ngpegasus <temma.fukaya@mokmok.dev>
2026-09-06 19:29:20 +0900

    feat: setup nix flake

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "";
+
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
+    flake-parts.url = "https://flakehub.com/f/hercules-ci/flake-parts/0";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+    git-hooks.url = "https://flakehub.com/f/cachix/git-hooks.nix/0";
+    git-hooks.inputs.nixpkgs.follows = "nixpkgs";
+    treefmt-nix.url = "https://flakehub.com/f/numtide/treefmt-nix/0";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-parts,
+      git-hooks,
+      treefmt-nix,
+      ...
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        git-hooks.flakeModule
+        treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        {
+          config,
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          _module.args.pkgs = import nixpkgs {
+            inherit system;
+          };
+
+          devShells.default = pkgs.mkShellNoCC {
+            inputsFrom = [ config.pre-commit.devShell ];
+          };
+
+          pre-commit.settings = {
+            hooks = {
+              deadnix.enable = true;
+              statix.enable = true;
+            };
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt.enable = true;
+            };
+          };
+        };
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+    };
+}
