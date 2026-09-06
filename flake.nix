103ecf495bd4a59accd356712532e733c7f1fc5a
pranc1ngpegasus <temma.fukaya@mokmok.dev>
2026-09-06 19:32:05 +0900

    feat: setup github actions

--- flake.nix
+++ flake.nix
@@ -43,6 +43,7 @@
 
           pre-commit.settings = {
             hooks = {
+              actionlint.enable = true;
               deadnix.enable = true;
               statix.enable = true;
             };
@@ -52,6 +53,7 @@
             projectRootFile = "flake.nix";
             programs = {
               nixfmt.enable = true;
+              yamlfmt.enable = true;
             };
           };
         };
